f3ebfc3046a9e336f2ba72a826d9455da20001bf
Elliott Morgan Jobson <emjobson@alumni.stanford.edu>
2019-07-26 18:33:16 -0700

    install pm2 locally

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       repo: 'git@github.com:emjobson/birthday-reminders.git',
       path: '/home/ubuntu/birthday-reminders', // will save contents of entire repo to this path --> this isn't a method of selecting which part of repo to clone
       'post-deploy':
-        'npm install --prefix ./backend && pm2 startOrRestart ./backend/ecosystem.config.js'
+        'npm install --prefix ./backend && pm2 startOrRestart ./backend/ecosystem.config.js' // verified that these work, cmd is run from top level of repository
     }
   }
 };
