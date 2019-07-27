f6e543a2c6c6ab31b4939fe3f5ae9c9cb348b603
Elliott Morgan Jobson <emjobson@alumni.stanford.edu>
2019-07-27 14:56:46 -0700

    updated host

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-217-223-241.us-east-2.compute.amazonaws.com', // careful, this seems to change upon logout of AWS?
+      host: 'ec2-3-17-65-28.us-east-2.compute.amazonaws.com', // careful, this seems to change upon logout of AWS?
       key: '~/.ssh/birthdays.pem',
       ref: 'origin/master',
       repo: 'git@github.com:emjobson/birthday-reminders.git',
