a4cabb9494df374fcd6942e4def55186dd14d64c
Elliott Morgan Jobson <emjobson@alumni.stanford.edu>
2019-07-26 17:38:05 -0700

    setup PM2

---
+++ backend/src/ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: './index.js'
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-217-223-241.us-east-2.compute.amazonaws.com', // careful, this seems to change upon logout of AWS?
+      key: '~/.ssh/birthdays.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:emjobson/birthday-reminders.git',
+      path: '/home/ubuntu/birthday-reminders',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
